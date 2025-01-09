66f740d99783b1607879cb3b35d170f76262b21e
Alejandro Ramirez <faramirezs@gmail.com>
2025-01-09 18:17:04 +0100

    last changes tree_node.h

--- headers/tree_node.h
+++ headers/tree_node.h
@@ -6,7 +6,6 @@
 
 typedef struct s_cmd t_cmd;
 typedef struct s_execcmd t_execcmd;
-typedef struct s_redircmd t_redircmd;
 typedef struct s_pipecmd t_pipecmd;
 typedef union u_node_value u_node_value;
 typedef struct s_tree_node t_tree_node;
@@ -38,29 +37,6 @@ struct s_execcmd
 	char **args;
 };
 
-struct s_redircmd
-{
-	struct s_redir *prev;
-	int     redir_type;      // REDIR_IN, REDIR_OUT, APPEND_OUT, HEREDOC
-	int     redir_i;         // Order of redirection
-	char    *target;         // Filename or delimiter
-	int     target_type;     // TARGET_FILENAME, TARGET_PATHNAME, TARGET_ENV_PATHNAME, TARGET_DELIMITER
-	int     target_token_type;
-	char	**args;
-	// Execution data
-	char    *exec_path;      // Path for execution
-	char    *exec_file;      // Actual file to execute on
-	char    *file_input;     // File content as single string
-	char    **split_input;   // File content as lines
-
-	// File handling
-	int     flags;          // O_RDONLY, O_WRONLY, O_APPEND, etc.
-	int     fd;            // File descriptor
-	int    close_fd;      // Resource management flag
-	int     error_code;    // For storing specific error states
-
-	struct s_redir *next;
-};
 
 struct s_pipecmd
 {
