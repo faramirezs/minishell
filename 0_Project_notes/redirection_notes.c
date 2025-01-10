62d538694aee77935cfd6d10fd8cb19e3958d694
Alejandro Ramirez <faramirezs@gmail.com>
2025-01-10 19:10:09 +0100

    Redirection strcuts AST

--- 0_Project_notes/redirection_notes.c
+++ 0_Project_notes/redirection_notes.c
@@ -1,19 +1,27 @@
-/*The redir struct contains this data:
-		- redir_type: which is the type of redirection
-		- redir_i: index of the redirection (this might be useful in our execution to understand if we are executing in the right order when having more than 1 redirs)
-		- target: which is the name of the target where the redirection executes(a file, path to a file or delimiter).
-		- target type: which is the type of the target. Input, output and append redir are always followed by files or path to files, heredoc are ONLY followed by a delimiter.
-		- target_token_type: type of the token that becomes the redir's target, this info is helpful for understanding already if we have a path or file in the case we have an output, input or heredoc redirection.
-		- s_redir *next: pointer to the next redirection (if we have just one redir, next is NULL)
-		- s_redir *prev: pointer to the prev redirection (for the first/head redir struct, prev is NULL)
 
-		When we are going to execute redirections, I think we should add this data to each redirection as well:
-		- exec_path: name of the path where we have to execute the redirection, if target type is path_to_file exec_path will be the path of the target, if target_type is file the exec_path is the path where we are located (when starting the shell or navigating in directories via cd)
-		- exec_file: the actual file which has "target" as file name. If the file does not exist we create it and name it as "target" (if target type is not word we have to get the last word in path (comes after last /). We then execute on this file, we read if it's from an output redir, or if it's input or append redir we have to check first if the file already has text that needs to be overwritten or appended to.
-		- file_input: the data that is inside the file, we can store everything together in a string at the beginning
-		- split_input: the file input split in different lines if we encounter a new line (\n)
-		- flags: flags that are used wwith the open function, which we will use when executing redirections (each redirection type requires different flags)
-		- int fd: file descriptor for error handling when we open the file and process its input
-		- bool close_fd: Indicates if the fd should be closed after use. Undure if we actually need this for error handling
+/*
+The s_redircmd struct represents a single redirection command in the AST.
 
+Fields:
+    - redir_type: Specifies the type of redirection (e.g., input, output, append).
+      This uses the t_token_type enum to define the redirection type.
+    - target: The target of the redirection. This can be a file name, path, or stream name.
+    - target_fd: The file descriptor associated with the target. This specifies
+      the descriptor to which the redirection applies (e.g., stdin, stdout).
+    - flags: Flags to be used with the open function. Different types of redirections
+      (input, output, append) require specific flags for file access.
+    - error_code: Used to store error codes that occur during redirection processing.
+      This helps in diagnosing issues such as file not found, permission denied, etc.
+
+
+This structure is designed to integrate into the AST, with each command node potentially
+containing a linked list of s_redircmd instances for managing its associated redirections.
 */
+
+struct s_redircmd {
+    t_token_type redir_type;          // Use enum for redirection types (e.g., input, output, append).
+    char *target;            // Target file or stream.
+    int target_fd;           // Target file descriptor.
+    int flags;               // File open flags.
+    int error_code;          // Error code during processing.
+};
--- headers/redirections.h
+++ headers/redirections.h
@@ -6,7 +6,7 @@
 /*   By: alramire <alramire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/10 17:40:19 by alramire          #+#    #+#             */
-/*   Updated: 2025/01/10 18:26:23 by alramire         ###   ########.fr       */
+/*   Updated: 2025/01/10 18:55:08 by alramire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,64 +15,7 @@
 
 # include "tree_node.h"
 
-/*
-	TARGET_FILENAME, 		// 0 - if token_type of the target is word or cmd
-	TARGET_PATHNAME, 		// 1 - if token is REL, ABS path (which can end or not with a file)
-	TARGET_ENV_PATHNAME,	// 2 - if token is an env_variable (once we have the nodes we check if the env_var is a valid path, like $PATH/file for example)
-	TARGET_DELIMITER		// 3 - if redir_type is HEREDOC
- */
-/*
-typedef enum e_target_type
-{
-	TARGET_FILENAME,
-	TARGET_PATHNAME,
-	TARGET_ENV_PATHNAME,
-} t_target_type;
- */
 
-/*
-	struct s_redir *prev;
-	int     redir_type;      // REDIR_IN, REDIR_OUT, APPEND_OUT, HEREDOC
-	int     redir_i;         // Order of redirection
-	char    *target;         // Filename or delimiter
-	int     target_type;     // TARGET_FILENAME, TARGET_PATHNAME, TARGET_ENV_PATHNAME, TARGET_DELIMITER
-	int     target_token_type;
-	char	**args;
-	// Execution data
-	//char    *exec_path;      // Path for execution
-	char    *exec_file;      // Actual file to execute on
-	char    *file_input;     // File content as single string
-	char    **split_input;   // File content as lines
-
-	// File handling
-	int     flags;          // O_RDONLY, O_WRONLY, O_APPEND, etc.
-	int     fd;            // File descriptor
-	int    close_fd;      // Resource management flag
-	int     error_code;    // For storing specific error states
-
-	struct s_redir *next;
- */
-/*
-struct						s_redircmd
-{
-	struct s_redir			*prev;
-	int						redir_type;
-	int						redir_i;
-	char					*target;
-	int						target_type;
-	int						target_token_type;
-	char					**args;
-	char					*exec_file;
-	char					*file_input;
-	char					**split_input;
-	int						flags;
-	int						fd;
-	int						close_fd;
-	int						error_code;
-
-	struct s_redir			*next;
-};
- */
 t_tree_node					*parse_redir(t_scanner *scanner, t_args *cmd_args);
 int							check_redir(t_scanner *scanner);
 int							determine_target_type(const char *target);
