0329cf4a86191a97a3330f1446644f7d1b260695
Alejandro Ramirez <faramirezs@gmail.com>
2025-02-15 18:05:20 +0100

    norm

--- execution/exec.c
+++ execution/exec.c
@@ -6,17 +6,79 @@
 /*   By: alramire <alramire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 16:59:59 by alramire          #+#    #+#             */
-/*   Updated: 2025/02/14 18:06:48 by alramire         ###   ########.fr       */
+/*   Updated: 2025/02/15 18:00:51 by alramire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/minishell.h"
-
-#define FORKED_CHILD 0
 
 void	cleanup(t_tree_node *node, int exit_code)
 {
 	free_tree_node(node);
 	clear_history();
 	exit(exit_code);
+}
+
+int	setup_pipe_redirection(t_context *ctx, int saved_stdin, int saved_stdout,
+		t_tree_node *node)
+{
+	if (ctx->fd[0] != STDIN_FILENO)
+	{
+		if (dup2(ctx->fd[0], STDIN_FILENO) == -1)
+		{
+			perror("dup2");
+			close(saved_stdin);
+			close(saved_stdout);
+			cleanup(node, 1);
+		}
+		close(ctx->fd[0]);
+	}
+	if (ctx->fd[1] != STDOUT_FILENO)
+	{
+		if (dup2(ctx->fd[1], STDOUT_FILENO) == -1)
+		{
+			perror("dup2");
+			fprintf(stderr, "setup_pipe\n");
+			close(saved_stdin);
+			close(saved_stdout);
+			cleanup(node, 1);
+		}
+		close(ctx->fd[1]);
+	}
+	return (0);
+}
+
+int	save_std_fds(int *saved_stdin, int *saved_stdout, t_tree_node *node)
+{
+	*saved_stdin = dup(STDIN_FILENO);
+	*saved_stdout = dup(STDOUT_FILENO);
+	if (*saved_stdin == -1 || *saved_stdout == -1)
+	{
+		perror("dup");
+		cleanup(node, 1);
+		return (1);
+	}
+	return (0);
+}
+
+void	restore_std_fds(int saved_stdin, int saved_stdout, t_tree_node *node)
+{
+	if (dup2(saved_stdin, STDIN_FILENO) == -1)
+	{
+		perror("dup2");
+		fprintf(stderr, "restore_std_fds1\n");
+		close(saved_stdin);
+		close(saved_stdout);
+		cleanup(node, 1);
+	}
+	if (dup2(saved_stdout, STDOUT_FILENO) == -1)
+	{
+		perror("dup2");
+		fprintf(stderr, "restore_std_fds\n");
+		close(saved_stdin);
+		close(saved_stdout);
+		cleanup(node, 1);
+	}
+	close(saved_stdin);
+	close(saved_stdout);
 }
--- headers/exec.h
+++ headers/exec.h
@@ -6,7 +6,7 @@
 /*   By: alramire <alramire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/15 17:12:47 by alramire          #+#    #+#             */
-/*   Updated: 2025/02/15 17:15:34 by alramire         ###   ########.fr       */
+/*   Updated: 2025/02/15 18:03:37 by alramire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,8 @@ int		exec_command(t_tree_node *node, t_context *ctx);
 int		exec_pipe(t_tree_node *node, t_context *ctx);
 int		exec_redir(t_tree_node *node, t_context *ctx);
 int		handle_heredoc(t_redircmd *rcmd);
-
+int		setup_pipe_redirection(t_context *ctx, int saved_stdin,
+			int saved_stdout, t_tree_node *node);
+int		save_std_fds(int *saved_stdin, int *saved_stdout, t_tree_node *node);
+void	restore_std_fds(int saved_stdin, int saved_stdout, t_tree_node *node);
 #endif
