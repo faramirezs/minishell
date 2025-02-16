f257f306a7cffaecf06119f350f47bdfee98a9eb
me11st <chokoboko@gmail.com>
2025-02-16 10:11:51 +0100

    finalish

--- libft/ft_strjoin_free_s1.c
+++ libft/ft_strjoin_free_s1.c
@@ -14,23 +14,29 @@
 
 char	*ft_strjoin_free_s1(char *s1, const char *s2)
 {
-	char	*new_str;
-	size_t	len1;
-	size_t	len2;
+	char    *new_str;
+	size_t  len1;
+	size_t  len2;
 
-	len1 = 0;
-	len2 = 0;
-	if (s1)
+	if (!s1)
+		len1 = 0;
+	else
 		len1 = ft_strlen(s1);
-	if (s2)
+	if (!s2)
+		len2 = 0;
+	else
 		len2 = ft_strlen(s2);
 	new_str = malloc(len1 + len2 + 1);
 	if (!new_str)
+	{
+		free(s1);
 		return (NULL);
+	}
 	if (s1)
-		strcpy(new_str, s1);
+		ft_memcpy(new_str, s1, len1);
 	if (s2)
-		strcpy(new_str + len1, s2);
+		ft_memcpy(new_str + len1, s2, len2);
+	new_str[len1 + len2] = '\0';
 	free(s1);
 	return (new_str);
 }
